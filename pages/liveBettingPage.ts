a8e5787f73849237641b847121fb974cce73dab1
AlexandarStoychev <alexandar.stoichev@gmail.com>
2024-12-13 02:48:20 +0200

    added readme

--- pages/liveBettingPage.ts
+++ pages/liveBettingPage.ts
@@ -15,7 +15,7 @@ export class LiveBettingPage {
   }
 
   async getAllLiveEventsAndSelectARandomOne() {
-    const numberOfEvents = await this.liveEventsLocator.count();
+    // choosing an event is hardcoded because the events are way too different
 
     // const randomIndex = faker.number.int({
     //   min: 0,
@@ -30,6 +30,7 @@ export class LiveBettingPage {
 
     await this.valuesInsideEventPage.nth(0).click();
 
+    // Playwright is trying to get the value too fast and the result is undefined
     await this.page.waitForTimeout(500);
 
     const valueOnTheBetslip = await this.getTextFromBetslip();
@@ -40,15 +41,21 @@ export class LiveBettingPage {
     const betValue = await this.valuesInsideEventPage.nth(0).textContent();
     await this.valuesInsideEventPage.nth(0).click();
 
+    // Even with this big wait, sometimes the odds are not changing for 10 or more seconds
     await this.page.waitForTimeout(10000);
 
+    // This assert is to check that a change in odds have actually happened
     const updatedBetOdd = await this.valuesInsideEventPage.nth(0).textContent();
     expect(updatedBetOdd).not.toBe(betValue);
+
+    // This assert is to check if the new value equals to the value on the betslip
     const valueOnTheBetslip = await this.getTextFromBetslip();
     expect(valueOnTheBetslip).toEqual(betValue);
   }
 
   async getTextFromBetslip() {
+    // Playwright was not able extract the value from this locatior with its "textContect" method,
+    // so page.evaluate had to be used
     return await this.page.evaluate(() => {
       const element = document.querySelector(".betslip-pick-odds__value");
       return element?.textContent?.trim().toString();
